71c51c359cd89f00c1fc39968474bde8ba71eb64
XingKong <rxy19961107@163.com>
2021-04-27 22:22:52 +0800

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -2,6 +2,7 @@
 
 
 def sendmail():
+    """发送邮件提示"""
     import smtplib
     from email.mime.text import MIMEText
     from email.header import Header
@@ -17,13 +18,12 @@ def sendmail():
     sender = '1137813060@qq.com'        # 由此用户代发
     receivers = ['1137813060@qq.com']   # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
     
-    message = MIMEText('已识别到问题，您的主机将于五分钟后自动关机...', 'plain', 'utf-8')
+    message = MIMEText('您的主机状态良好，继续运行中...', 'plain', 'utf-8')
     message['From'] = Header("您的主机", 'utf-8')       # 发送者
     message['To'] =  Header("主人", 'utf-8')            # 接收者
 
-    subject = '自动关机(程序自动发送)'
+    subject = '正常运行提示(程序自动发送)-ZM'
     message['Subject'] = Header(subject, 'utf-8')
-    
     
     try:
         smtpObj = smtplib.SMTP(mail_host, mail_port)    # mail_port 为 SMTP 端口号
@@ -42,14 +42,13 @@ import time
 import status
 
 if __name__=='__main__':
-    
     while True:
         time.sleep(1800)        # 半个小时(1800秒)运行一次
         shutdowns = status.network_status()
         if(shutdowns == 1):
-            print("半小时后自动关机")
+            print("五分钟后自动关机")
             os.system('shutdown -s -t {}'.format(300))     # 五分钟后关机
-            sendmail()      # 发送邮件提示
             break
         else:
             print("状态正常，继续运行")
+            sendmail()      # 发送邮件提示
